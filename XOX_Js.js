d57f5dfd7cea901c6f3abe7927293d721857cbde
Sanketh S <sanketshettyjan18@gmail.com>
2025-04-05 23:17:54 +0530

    Update XOX_Js.js

--- XOX_Js.js
+++ XOX_Js.js
@@ -1,6 +1,4 @@
-
-
-
+et turn0=true;
 
 const winPattern=[
     [0,1,2],
@@ -13,4 +11,36 @@ const winPattern=[
     [2,4,6],
 ];
 
+const resetGame=()=>{
+    turn0=true;
+    enableBoxes();
+    msg_container.classList.add("hide");
 
+}
+
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+        if (turn0){
+            box.innerText="0";
+            turn0=false;
+        }else{
+            box.innerText="X";
+            turn0=true;
+        }
+        box.disabled=true;
+        checkWinner();
+    });
+});
+
+const disabledBoxes=()=>{
+    for(let box of boxes){
+        box.disabled=true;
+    }
+};
+
+const enableBoxes=()=>{
+    for(let box of boxes){
+        box.disabled=false;
+        box.innerText="";
+    }
+};
